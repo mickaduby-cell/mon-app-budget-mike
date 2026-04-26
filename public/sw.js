4cb71cdd9b4b2bbf77cc21f11d8efb863d9f1f89
mickaduby-cell <mickaduby@hotmail.fr>
2026-04-26 20:23:17 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,15 @@
 const CACHE_NAME = "budget-app-v3";
 
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+      Promise.all(
+        keys.map((key) => key !== CACHE_NAME && caches.delete(key))
+      )
     )
   );
 
@@ -15,7 +17,5 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
