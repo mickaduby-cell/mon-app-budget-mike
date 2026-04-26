19ac4069d6909632d7b3aac655ab7d41b9a9be47
mickaduby-cell <mickaduby@hotmail.fr>
2026-04-26 18:07:24 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "budget-app-v1";
+const CACHE_NAME = "budget-app-v2";
 
 const urlsToCache = [
   "/",
@@ -6,11 +6,29 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting(); // force activation immédiate
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
