90776c0d6adf0790491667dfb9aa89f8d1c46f17
mickaduby-cell <mickaduby@hotmail.fr>
2026-04-26 20:27:55 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,27 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", async () => {
-    const registrations = await navigator.serviceWorker.getRegistrations();
-
-    await Promise.all(
-      registrations.map((registration) => {
-        const url =
-          registration.active?.scriptURL ||
-          registration.installing?.scriptURL ||
-          registration.waiting?.scriptURL ||
-          "";
-
-        if (!url.endsWith("/sw.js")) {
-          return registration.unregister();
-        }
-
-        return Promise.resolve();
-      })
-    );
-
-    navigator.serviceWorker.register("/sw.js");
-  });
-}
